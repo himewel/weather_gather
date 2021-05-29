ba13229f5f85cdb854c6846876647a796f52ad81
Welbert Castro <himewel@users.noreply.github.com>
2021-05-28 21:47:06 -0300

    Add help.pl

---
+++ terraform/help.pl
@@ -0,0 +1,21 @@
+%help;
+
+while(<>) {
+    if(/^([a-z0-9_-]+):.*\#\#(?:@(\w+))?\s(.*)$/) {
+        push(@{$help{$2}}, [$1, $3]);
+    }
+};
+
+print "usage: make [target] [VARIABLE=VALUE]\n\n";
+
+print "parameters:\n";
+printf("  %-20s %s\n", "BQ_DATASET", "Dataset ID to upload the data in BigQuery");
+printf("  %-20s %s\n", "GOOGLE_CLOUD_PROJECT", "Project name with the GCS bucket and BigQuery dataset");
+printf("  %-20s %s\n", "GCS_BUCKET", "Bucket name in Cloud Storage");
+print "\n";
+
+for ( sort keys %help ) {
+    print "$_:\n";
+    printf("  %-20s %s\n", $_->[0], $_->[1]) for @{$help{$_}};
+    print "\n";
+}
